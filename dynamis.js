7fa4ff85bebe2283e179fad021282914595f9c7e
sondos-braim <sondos9811@gmail.com>
2020-09-04 22:34:56 +0300

    I made it more vibrant

--- dynamis.js
+++ dynamis.js
@@ -1,32 +1,27 @@
+function name(){
+    var fName=prompt("enter your First name");
+    var lName=prompt("enter your Last name");
+ document.write("Hey, "+ fName +"  "+ lName+ ", how are you today?");
+ }
+ name();
 
-var lName;
-var fName;
-fName =prompt("enter your First name");
-lName =prompt("enter your Last name");
-function sayyourname(first,Last){
-    return lName,fName;
-}
-document.write("Hey, "+ fName +"  "+ lName+ ", how are you today?");
-
+ function books(){
+ var userinput=prompt("How many books do you read a month?");
  
-var userinput
-userinput=prompt("How many books do you read a month?");
-function read(book){
-    var greetings;
-    while(isNaN(userinput)||userinput == '') { 
-        userinput=prompt("Please insert a number");
+     var greetings;
+     while(isNaN(userinput)||userinput == '') { 
+         userinput=prompt("Please insert a number");
+     }
+       if (userinput>10) {
+         greetings= 'Great Job!'; 
+     } else {
+         greetings='Try harder';
+     }
+ document.write('<h3>' + greetings + '</h3>');
+    
     }
-      if (book>10) {
-        greetings= 'Great Job!'; 
-    } else {
-        greetings='Try harder';
-    }
-    return greetings;
-}
-read(userinput);
-// document.write();
-document.write('<h3>' + read(userinput) + '</h3>');
 
+ books();
 
 
 
